ea1aeeb90984d036b55e034efe8dcc96668665bb
Tejas Kumar Singh <95142956+tejaskrsingh@users.noreply.github.com>
2021-11-29 15:42:29 +0530

    Create arithematicoperator.c

---
+++ arithematicoperator.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+void main()
+{
+int a, b;
+a=5;
+b=10;
+int c=a+b;
+printf("\nThe sum is: %d",c);
+int d=a-b;
+printf("\nThe difference is: %d",d);
+int e=a%d;
+printf("\nThe reminder is: %d",e);
+int f=a/b;
+printf("\nThe quotient is: %d",f);
+int g=a*b;
+printf("\nThe multiplcation is: %d",c);
+}
