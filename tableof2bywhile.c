3b48f4f905f9d913c70d8d7ddf4abf96c9a8d82b
Tejas Kumar Singh <95142956+tejaskrsingh@users.noreply.github.com>
2021-11-29 16:54:42 +0530

    Create tableof2bywhile.c

---
+++ tableof2bywhile.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+void main()
+{
+ int i=1;
+ int t;
+ int a=2;
+ while(i<=10)
+ {
+     t=2*i;
+     printf("\n %d * %d = %d", a,i,t);
+     i++;
+ }
+}
