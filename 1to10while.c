b9fba854eb89a04b571887ecbc37f58b7e6a2eb9
Tejas Kumar Singh <95142956+tejaskrsingh@users.noreply.github.com>
2021-11-29 16:25:29 +0530

    Create 1to10while.c

---
+++ 1to10while.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+void main()
+{
+int i=0;
+while(i<=10)
+{
+    printf("%d\n",i);
+    i++;
+}
+}
